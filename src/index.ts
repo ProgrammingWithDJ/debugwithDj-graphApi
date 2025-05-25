09b3f1092d8d9c30af8675125958042fd3043bd0
Dipesh Jadhav <dipxsss@gmail.com>
2025-05-25 13:44:07 +0530

    Update ts file

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,27 @@
 import { getInput, setFailed } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 
-const name = getInput("name");
+export async function run() {
+  const token = getInput("gh-token");
+  const label = getInput("label");
 
-console.log(`Hello ${name}`);
+  const octokit = getOctokit(token);
+  const pullRequest = context.payload.pull_request;
 
-async function run() {
-    const token = getInput("gh-token");
-    const label = getInput("label");
-
-    const octokit = getOctokit(token);
-    const pullRequest = context.payload.pull_request;
-
-    try {
-        if (!pullRequest) {
-            throw new Error("No pull request found in the context");
-        }
-
-        await octokit.rest.issues.addLabels({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            issue_number: pullRequest.number,
-            labels: [label],
-        });
-    } catch (error) {
-       setFailed((error as Error)?.message ?? "Unknown error");
+  try {
+    if (!pullRequest) {
+      throw new Error("This action can only be run on Pull Requests");
     }
+
+    await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label],
+    });
+  } catch (error) {
+    setFailed((error as Error)?.message ?? "Unknown error");
+  }
 }
 
 run();
